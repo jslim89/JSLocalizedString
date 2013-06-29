9d021823110ed7d0528f6b5aff1949a4f0e4adc1
jslim89 <jslim89@gmail.com>
2013-06-29 10:40:06 +0800

    Initial Commit

---
+++ JsLocalizedString/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JsLocalizedString
+//
+//  Created by JS Lim on 6/29/13.
+//  Copyright (c) 2013 Js Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JsLocalizedString/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JsLocalizedString
+//
+//  Created by JS Lim on 6/29/13.
+//  Copyright (c) 2013 Js Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
